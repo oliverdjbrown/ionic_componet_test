cfbe75c64344bc480b5ae56e5dc0b28a789e0a16
Oliver Brown <akuma_shadow@hotmail.com>
2019-11-03 08:49:48 -0400

    Finalizacion de implementacion Slide Content

--- src/app/pages/slide/slide.page.ts
+++ src/app/pages/slide/slide.page.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slide',
-  templateUrl: './slide.page.html',
+  templateUrl: `./slide.page.html`,
   styleUrls: ['./slide.page.scss'],
 })
 export class SlidePage implements OnInit {
-
-  constructor() { }
+    slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+  constructor() {}
 
   ngOnInit() {
   }
-
 }
